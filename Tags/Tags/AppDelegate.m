e95f12fe916f2585b46a5709b6fc87c5f0ecd7b0
ZCH <406639750@qq.com>
2016-08-16 17:20:30 +0800

    打上自己的标记

--- Tags/Tags/AppDelegate.m
+++ Tags/Tags/AppDelegate.m
@@ -19,7 +19,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    [NSThread sleepForTimeInterval:3.0];
+    [NSThread sleepForTimeInterval:2.0];
     
     
     self.window = [[UIWindow alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, kScreenHeight)];
@@ -34,8 +34,10 @@
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
+    
+    
+    
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -44,7 +46,10 @@
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    
+    
+    
+    
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
